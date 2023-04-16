16a3e03ff4d879504ca9b9e1fd9c84af2e2d682e
Dario Castañé <dcastane@coopanio.com>
2023-04-16 21:39:54 +0200

    feat: add HTTP Basic auth
    Let's dogfood Mapado from the beginning.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env.production?
 end
